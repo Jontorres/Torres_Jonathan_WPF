2bdd3aeaa1847998d02f42dd84d2f059d3e03e6f
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 15:21:38 -0400

    Expressions Age
    I commented out everything from the begging down to expression of age
    because I am going to continue
    to use this js file for these lessons so that I may look back at them
    later. Also learned how to subtract a year if I did not have a birthday
    yet.

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -15,19 +15,19 @@ alert(yearBorn);
 
 //Declaring Variables
 //Declare before you define.
-var age;
+//var age;
 
 // Defining Variables 
 //Variables of Different Types, Numbers, Booleans and Strings
-age = 38;// Number variable
+//age = 38;// Number variable
 //storage container = value
 
-var yearOfBirth = 1970;//declaring and defining on the same line.
+//var yearOfBirth = 1970;//declaring and defining on the same line.
 
-var isStudent = false;//Boolean Variable
+//var isStudent = false;//Boolean Variable
 //Boolean Variables are either On or Off, False or True
 
-var firstName = "Joanthan";//String variable
+//var firstName = "Joanthan";//String variable
 //needs to have quotes either single or double
 // becarful when using a single quote
 
@@ -43,24 +43,27 @@ is to use the backslach to show that the ' is not the end of a quote.
 
 //Arrays
 
-var muppetNames = ["Kermit", "Miss Piggy"]; //this makes a list, 0,1,2,3
+//var muppetNames = ["Kermit", "Miss Piggy"]; //this makes a list, 0,1,2,3
 //index starts with 0 so kermit would be 0, piggy would be 1 and so on
 //, "Gonzo", "Rizzo" took these out
 //console.log(muppetNames[2]);
-muppetNames[1] = "Rizzo"; //this is if you want to change miss piggy's name
-muppetNames[2]= "Fozzy";//adding this to fozzy
+//muppetNames[1] = "Rizzo"; //this is if you want to change miss piggy's name
+//muppetNames[2]= "Fozzy";//adding this to fozzy
 // can also use var instead of the numbers 
- var num = 1;
+// var num = 1;
 
 //console.log(muppetNames[num]);
 
 //Expressions
- var a = 2;//sets up our variable a and defiens it as 2
+// var a = 2;//sets up our variable a and defiens it as 2
 //a + 3; add 3 to A suppose to  but will not because it is not storing it 
 //so to make add up to five you have to add this--
-var b; //if want to store the value of 5 in B instead of A
-b = a + 3;// this stores five into the A variable by usng = which is an assingment operator 
- console.log(b);
+//var b; //if want to store the value of 5 in B instead of A
+//b = a + 3;// this stores five into the A variable by usng = which is an assingment operator 
+// console.log(b);
  // A's value would now be 2 and B's value is now 5
 
-
+// Expressions - Age example
+var yearBorn =1991;
+var age = 2014 - yearBorn - 1; //if you have not had a birthday yet
+console.log(age);
