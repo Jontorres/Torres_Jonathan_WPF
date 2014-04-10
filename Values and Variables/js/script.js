22a41e04eba278acebe126852dd6018be5bc1a2b
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 14:23:40 -0400

    declaring and defining
    Learned about declaring a variable and defining a variable

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -1,9 +1,25 @@
 //JavaScript
+//Values and Variables
+/*
 var yearBorn = 1955;
+*/
 
 /*
 console.log("Hellow World!");
 console.log(yearBorn);
 	*/
 
-alert("Helow World!");
+/*
+alert(yearBorn);
+*/
+
+//Declaring Variables
+//Declare before you define.
+var age;
+
+// Defining Variables 
+age = 38;
+//storage container = value
+
+var yearOfBirth = 1970;//declaring and defining on the same line.
+
