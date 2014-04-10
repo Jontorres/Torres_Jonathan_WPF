74474bfa855ce52b08e560e4208f047cf9b2ec02
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 16:16:55 -0400

    Using Arrays in Expressions
    learned how to add arrays together

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -116,7 +116,13 @@ console.log(perimeter);
 */
 
 
-
+/*
 //Expression- Using Arrays in Expressions
+var orangeBins = [234, 567, 883];
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
+*/
+
+
 
 
