4b822f06a93d02cddbdf1d5240d8ffa27d6d86ba
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 14:40:32 -0400

    Different types of variables

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -18,8 +18,25 @@ alert(yearBorn);
 var age;
 
 // Defining Variables 
-age = 38;
+//Variables of Different Types, Numbers, Booleans and Strings
+age = 38;// Number variable
 //storage container = value
 
 var yearOfBirth = 1970;//declaring and defining on the same line.
 
+var isStudent = false;//Boolean Variable
+//Boolean Variables are either On or Off, False or True
+
+var firstName = "Joanthan";//String variable
+//needs to have quotes either single or double
+// becarful when using a single quote
+
+//var phrase = 'I don't know'; this would be wrong
+
+//to fix you can do this 
+var phrase = 'I don\'t know';
+// or you can do this 
+var phrase = I don\'t know \n You\'ll have to speak with the professor';
+/* \n is just making the second half be shown on a new line what is important 
+is to use the backslach to show that the ' is not the end of a quote.
+*/
