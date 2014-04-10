50d62eab102c19bc41757a25d05ffaad8896bfa1
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 14:57:07 -0400

    Arrays
    Showing how arrays work

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -34,9 +34,22 @@ var firstName = "Joanthan";//String variable
 //var phrase = 'I don't know'; this would be wrong
 
 //to fix you can do this 
-var phrase = 'I don\'t know';
+//var phrase = 'I don\'t know';
 // or you can do this 
-var phrase = I don\'t know \n You\'ll have to speak with the professor';
+//var phrase = I don\'t know \n You\'ll have to speak with the professor';
 /* \n is just making the second half be shown on a new line what is important 
 is to use the backslach to show that the ' is not the end of a quote.
 */
+
+//Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy"]; //this makes a list, 0,1,2,3
+//index starts with 0 so kermit would be 0, piggy would be 1 and so on
+//, "Gonzo", "Rizzo" took these out
+//console.log(muppetNames[2]);
+muppetNames[1] = "Rizzo"; //this is if you want to change miss piggy's name
+muppetNames[2]= "Fozzy";//adding this to fozzy
+// can also use var instead of the numbers 
+ var num = 1;
+
+console.log(muppetNames[num]);
