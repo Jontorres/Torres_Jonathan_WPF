321a80aadcc5eb07b85003f1403a6859a6fb8849
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 17:43:28 -0400

    Fifth Commit
    I added in the console.log a message explaining sparkys
    age, I also changed humanAge to sparkyAge so that it
    would match the question better

--- Expression_Worksheet/js/script.js
+++ Expression_Worksheet/js/script.js
@@ -8,10 +8,12 @@
 //For every 1 human year dogs age goes up by 7 
 
 //this is Sparky's age in human Years
-var humanAge = 10;
+var sparkyAge = 10;
 //this is the dog years that will need to be multiplied for correct age
 var dogYears = 7;
 //multiply the humanAge with the dogYears
-var result = humanAge * dogYears;
+var result =  sparkyAge * dogYears;
 //This will print out the result
 console.log(result);
+//This is Sparky's age x in human years which is X in dog years
+console.log("Sparky is "+sparkyAge+" human years old which is "+result+" in dog years")
