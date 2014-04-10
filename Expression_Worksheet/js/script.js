08bc734c15ad375b266f147016a2e30b904a6440
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 17:21:49 -0400

    First commit
    This shows Sparky’s age in human years

--- Expression_Worksheet/js/script.js
+++ Expression_Worksheet/js/script.js
@@ -1,3 +1,13 @@
 //Jonathan Torres
 //April 10, 2014
 //Expressions Worksheet
+
+
+//Dog Years
+//Caculate how old Sparky the pit bull is in dog Years
+//For every 1 human year dogs age goes up by 7 
+
+//this is Sparky's age in human Years
+var humanAge = 10;
+
+
