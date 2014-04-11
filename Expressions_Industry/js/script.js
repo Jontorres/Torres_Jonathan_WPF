c1f3c75a398d6e2ed241d99db2a1247fafc2b86d
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 23:29:09 -0400

    1st commit
    This alert tells the user that he/she will be participating in
    a height and weight activity to make sure they are in regulations

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,7 @@
 //Jonathan Torres
 //April 10,2014
 //Expressions_Industry
+
+
+//This is for army height and weight because our job depends on if we are in the army standard
+alert("Welcome to the Army, today you will be giving your height weight and age! First soldier up!");
