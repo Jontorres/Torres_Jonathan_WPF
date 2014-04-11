ffad302e507caa7ad134bfa99a2708debdc169af
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 21:53:09 -0400

    5th commit
    Now we will see how long the user is inactive on the days they do not
    workout

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -9,4 +9,6 @@ var days = prompt("How many days do you go to the gym in a week?");
 //The next prompt will find out how many hours the user works out at the gym
 var hours = prompt("How many hours a day do you work out?");
 //This will tell how many days you do not work out
-var noDays = prompt("how many days do you not work out in a week?");
+var noDays = prompt("How many days do you not work out in a week?");
+//This will tell how much time you do not do any activities on the days you do not work out
+var noHours = prompt("How much time do you sit on the days you do not work out?")
