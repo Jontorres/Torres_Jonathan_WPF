5ad62998ed598890c4d26683be3e9bc5a380884c
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 23:30:49 -0400

    2nd commit
    soldiers weight

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,3 +5,5 @@
 
 //This is for army height and weight because our job depends on if we are in the army standard
 alert("Welcome to the Army, today you will be giving your height weight and age! First soldier up!");
+//This prompt gets the soldiers weight
+var height =prompt("What is your weight in pounds?);
