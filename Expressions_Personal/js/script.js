588d0806e1bebefd8fd55e037eafdb297a87c00b
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 22:00:24 -0400

    8th commit
    changed the order of the code so that after putting the days and hours
    you work out you will see how much you work out a week

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -8,12 +8,13 @@
 var days = prompt("How many days do you go to the gym in a week?");
 //The next prompt will find out how many hours the user works out at the gym
 var hours = prompt("How many hours a day do you work out?");
+//this will now tell us the amount of exercise in a week the user does
+var exercice = days + hours;
+//checking to see if the expression works
+console.log(exercice);
+
 //This will tell how many days you do not work out
 var noDays = prompt("How many days do you not work out in a week?");
 //This will tell how much time you do not do any activities on the days you do not work out
 var noHours = prompt("How much time do you sit on the days you do not work out?")
 
-//this will now tell us the amount of exercise in a week the user does
-var exercice = days + hours;
-//checking to see if the expression works
-console.log(exercice);
