f945631b7378bd3eca23dc832426d5913ed868b7
Jontorres <jtstjun2212@yahoo.com>
2014-04-10 21:40:26 -0400

    2nd commit
    how many days in a week do you go to the gym

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,6 @@
 //Expressions_Personal
 
 //This will show how much time is spent at the gym in a week
+
+//This first prompt will gather the amount of days that the user goes to the gym
+var days = prompt("How many days do you go to the gym in a week?");
