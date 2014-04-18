08b3c55b921afc398d78b4727bd7a41dcd3d2764
Jontorres <jtstjun2212@yahoo.com>
2014-04-17 23:38:08 -0400

    7th commit
    I am changing the code because the first code was not working for me
    and was to complicated

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,18 +3,7 @@
 // Conditionals Personal
 
 
-//this will be my starting prompt to figure out what audience I am talking to 
+//this will be my starting prompt 
 var gender = prompt("Are you a boy or a girl?");
-// I will be using this variable to ask the next question for a boy
-var boy;
-// This will be the girls variable
-var girl;
-//now I am adding the if condition which will lead to the next question 
-if (gender ==="boy"){
-	boy = prompt("How many hours have you spent playing video games in your life?");
-
-	//added an else for if they answer as a girl
-}else{
-	//add the third question
-	girl + prompt("How many hours have you spent shopping?");
-}
+// I will be using this decide what the winner recieves
+var winner;
