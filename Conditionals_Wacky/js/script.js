a736494238ba4315d9d528bc8eb61f0bbaccd6a6
Jontorres <jtstjun2212@yahoo.com>
2014-04-18 01:38:21 -0400

    23rd commit
    this is the end of the code

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -20,4 +20,31 @@ var toons;
 
 // now the conditionals
 if (wacky == loonyToons[0]){
+	//each toon will have their fav quote 
+	//this is for bugs
+	toons = "Whats up doc!";
+}
+//this is for speedy 
+if (wacky == loonyToons[1]){
+	toons = "Andale, Andale, Arriba, Arriba!!!";
+
+}
+//this is for sylvester
+if(wacky == loonyToons[2]){
+	toons = "Sufferin Succotash!";
+
+}
+//this is for tweety
+if(wacky == loonyToons[3]){
+	toons = "I thought I saw a puddy tat?";
+}
+//this is for daffy duck
+if (wacky == loonyToons[4]){
+	toons = "You're Desthpicable";
+}
+
+//this will alert the user with the final message showing what they picked 
+alert(toons+ " Your favorite Loony Toons character is "+wacky+ " thanks for playing "+name+ " see you later "+alien+".");
+//this will print out to the console
+console.log(toons+ " Your favorite Loony Toons character is "+wacky+ " thanks for playing "+name+ " see you later "+alien+".");
 
