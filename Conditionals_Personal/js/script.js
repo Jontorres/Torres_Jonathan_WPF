259c34bbab8087885cb884d3246c148082c1098a
Jontorres <jtstjun2212@yahoo.com>
2014-04-17 22:10:44 -0400

    2nd commit
    boy variable

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,5 +5,5 @@
 
 //this will be my starting prompt to figure out what audience I am talking to 
 var gender = prompt("Are you a boy or a girl?");
-
-
+// I will be using this variable to ask the next question for a boy
+var boy;
