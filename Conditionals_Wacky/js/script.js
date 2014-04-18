d3791b309de7953993083e399ce54f2cd10926bd
Jontorres <jtstjun2212@yahoo.com>
2014-04-18 01:35:03 -0400

    18th commit

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,4 +6,5 @@
 alert("Welcome to Wacky Land!!!!");
 //A prompt for a name
 var name = prompt("First let me get your name please.");
-
+// a wacky question
+var alien = prompt("And are you a alien or a human?");
