e059ff362493cdb9d159e9f99b475afd150be7be
Jontorres <jtstjun2212@yahoo.com>
2014-04-18 00:05:25 -0400

    15th commit
    realized there were caps instead of lower case

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -9,7 +9,7 @@ var soldier = prompt("How many sit-ups can you do on a pt test?");
 //the variable the the sit up will be used by
 var score;
 // this is the ternary
-score = (soldier < 75) ? "Failed" : "Passed";
+score = (soldier < 75) ? "failed" : "passed";
 //this will alert the soldier
 alert("Soldier you have "+score+ " your PT test!");
 //this will print out to the console
