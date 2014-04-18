88bd789bd4403d2bd53a4cb86c2b034d7565105e
Jontorres <jtstjun2212@yahoo.com>
2014-04-17 22:09:28 -0400

    1st commit
    starting prompt

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,9 @@
 // Jonathan Torres
 // April 17, 2014
 // Conditionals Personal
+
+
+//this will be my starting prompt to figure out what audience I am talking to 
+var gender = prompt("Are you a boy or a girl?");
+
+
