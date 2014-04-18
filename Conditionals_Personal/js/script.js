f6b4507f1371c9848f2196d761f173bb17569987
Jontorres <jtstjun2212@yahoo.com>
2014-04-17 22:14:23 -0400

    4th commit
    if condition

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -9,3 +9,7 @@ var gender = prompt("Are you a boy or a girl?");
 var boy;
 // This will be the girls variable
 var girl;
+//now I am adding the if condition which will lead to the next question 
+if (gender ==="boy"){
+
+}
