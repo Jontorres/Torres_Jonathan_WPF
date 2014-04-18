65ddb1b3836cfc12e6347c3362f655ca1ed12478
Jontorres <jtstjun2212@yahoo.com>
2014-04-17 23:40:26 -0400

    8th commit

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,3 +7,11 @@
 var gender = prompt("Are you a boy or a girl?");
 // I will be using this decide what the winner recieves
 var winner;
+
+//This is the same if condition
+if (gender == "boy"){
+	//new statement gives the boy a game 
+	winner = "You won a new video game!";
+	
+
+}
