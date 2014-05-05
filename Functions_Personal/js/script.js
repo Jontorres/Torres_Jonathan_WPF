f4f0d6238640f28c2b2f398ded22d78757702b3d
Jontorres <jtstjun2212@yahoo.com>
2014-05-04 14:54:13 -1000

    4th commit
    now i have the console.log code in the function

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -8,4 +8,7 @@
 var foodDemand = function(food){
 	//In side the function I will have the code that will give the user their response
 	alert("I would like to eat "+ food+ ".");
+	//I wil also add in the console.log so that it will print out the answer in the console
+	console.log("I would like to eat "+ food+ ".");
 }
+
