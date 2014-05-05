88d49652df91093b7f900299ce11bce9a2274f82
Jontorres <jtstjun2212@yahoo.com>
2014-05-04 16:19:30 -1000

    13th commit
    Welcome alert

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,10 @@
 //Jonathan Torres
 //May 1, 2014
 //Functions Industry
+
+/*This code will tell you how much you made per month depending on the amount of 
+hours you worked in four weeks.
+*/
+
+// this alert will start off the code
+alert("Hi welcome to your new job. You will be getting paid 10 dollars per hour and will recieve a paycheck every two weeks, goodluck!");
