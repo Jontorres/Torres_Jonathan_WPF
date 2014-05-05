3e884066f59e26e9a5140bf54a0b32c6d16d1739
Jontorres <jtstjun2212@yahoo.com>
2014-05-04 16:23:35 -1000

    16th commit
    alert to tell how much you made each pay check

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -13,5 +13,7 @@ alert("Hi welcome to your new job. You will be getting paid 10 dollars per hour 
 var payCheck = function(hours){
 // now will add a variable that will * the hours you work with your wage 10 
 	var wage = hours * 10; 
-
+//now will add an alert that will tell you how much you made each pay check 
+	alert("You earned $"+ wage+ ".00" +" this paycheck.");
+	
 }
